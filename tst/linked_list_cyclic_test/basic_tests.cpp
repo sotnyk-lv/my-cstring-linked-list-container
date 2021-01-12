371526b1b9a7d150c17a8ec3434ae38f810a790b
Danylo Sahaidak <danylos2001@gmail.com>
2021-01-12 13:42:16 +0200

    All tests done.
    TODO: fix conversion from string literal to 'char *'

--- tst/linked_list_cyclic_test/basic_tests.cpp
+++ tst/linked_list_cyclic_test/basic_tests.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 
 #include "gtest/gtest.h"
 
@@ -393,6 +394,7 @@ TEST(list_index_of, single_element_list_index_of_not_present) {
     int index = StringListIndexOf(list, value);
 
     ASSERT_EQ(index, -1);
+    ASSERT_EQ(StringListSize(list), 1);
 
     char **arr;
     StringListToString(list, &arr);
@@ -498,5 +500,365 @@ TEST(list_index_of, multiple_elements_list_index_of_present) {
 
 
     free_string_array(arr, StringListSize(list));
+    StringListDestroy(&list);
+}
+
+TEST(list_remove_duplicates, empty_list_remove_duplicates) {
+    char **list = nullptr;
+    StringListInit(&list);
+    StringListRemoveDuplicates(list);
+
+    ASSERT_TRUE(list != nullptr);
+    ASSERT_TRUE(list[WORD] == nullptr);
+    ASSERT_TRUE(reinterpret_cast<char **>(list[1]) == list);
+    ASSERT_TRUE(reinterpret_cast<char **>(list[2]) == list);
+
+    StringListDestroy(&list);
+}
+
+TEST(list_remove_duplicates, single_element_list_remove_duplicates) {
+    char **list = nullptr;
+    StringListInit(&list);
+    StringListAdd(list, "b");
+    StringListRemoveDuplicates(list);
+
+    ASSERT_EQ(StringListSize(list), 1);
+
+    char **arr;
+    StringListToString(list, &arr);
+
+    ASSERT_TRUE(arr != nullptr);
+    ASSERT_STREQ(arr[0], "b");
+
+
+    free_string_array(arr, StringListSize(list));
+    StringListDestroy(&list);
+}
+
+TEST(list_remove_duplicates, multiple_element_list_remove_duplicates_no_duplicates) {
+    char **list = nullptr;
+    StringListInit(&list);
+    StringListAdd(list, "a");
+    StringListAdd(list, "b");
+    StringListAdd(list, "c");
+    StringListAdd(list, "d");
+    StringListAdd(list, "");
+    StringListAdd(list, "e");
+
+    StringListRemoveDuplicates(list);
+
+    ASSERT_EQ(StringListSize(list), 6);
+
+    char **arr;
+    StringListToString(list, &arr);
+
+    ASSERT_TRUE(arr != nullptr);
+    ASSERT_STREQ(arr[0], "a");
+    ASSERT_STREQ(arr[1], "b");
+    ASSERT_STREQ(arr[2], "c");
+    ASSERT_STREQ(arr[3], "d");
+    ASSERT_STREQ(arr[4], "");
+    ASSERT_STREQ(arr[5], "e");
+
+
+    free_string_array(arr, StringListSize(list));
+    StringListDestroy(&list);
+}
+
+TEST(list_remove_duplicates, multiple_element_list_remove_duplicates) {
+    char **list = nullptr;
+    StringListInit(&list);
+    StringListAdd(list, "a");
+    StringListAdd(list, "b");
+    StringListAdd(list, "c");
+    StringListAdd(list, "");
+    StringListAdd(list, "");
+    StringListAdd(list, "d");
+    StringListAdd(list, "");
+    StringListAdd(list, "e");
+    StringListAdd(list, "e");
+    StringListAdd(list, "b");
+
+    ASSERT_EQ(StringListSize(list), 10);
+
+    StringListRemoveDuplicates(list);
+
+    ASSERT_EQ(StringListSize(list), 6);
+
+    char **arr;
+    StringListToString(list, &arr);
+
+    ASSERT_TRUE(arr != nullptr);
+    ASSERT_STREQ(arr[0], "a");
+    ASSERT_STREQ(arr[1], "b");
+    ASSERT_STREQ(arr[2], "c");
+    ASSERT_STREQ(arr[3], "");
+    ASSERT_STREQ(arr[4], "d");
+    ASSERT_STREQ(arr[5], "e");
+
+
+    free_string_array(arr, StringListSize(list));
+    StringListDestroy(&list);
+}
+
+TEST(list_replace_in_strings, empty_list_replace_in_strings) {
+    char **list = nullptr;
+    StringListInit(&list);
+    char before[] = "a";
+    char after[] = "b";
+    StringListReplaceInStrings(list, before, after);
+
+    ASSERT_TRUE(list != nullptr);
+    ASSERT_TRUE(list[WORD] == nullptr);
+    ASSERT_TRUE(reinterpret_cast<char **>(list[1]) == list);
+    ASSERT_TRUE(reinterpret_cast<char **>(list[2]) == list);
+
+    StringListDestroy(&list);
+}
+
+TEST(list_replace_in_strings, empty_list_replace_in_strings_same_element) {
+    char **list = nullptr;
+    StringListInit(&list);
+    char before[] = "a";
+
+    StringListReplaceInStrings(list, before, before);
+
+    ASSERT_TRUE(list != nullptr);
+    ASSERT_TRUE(list[WORD] == nullptr);
+    ASSERT_TRUE(reinterpret_cast<char **>(list[1]) == list);
+    ASSERT_TRUE(reinterpret_cast<char **>(list[2]) == list);
+
+    StringListDestroy(&list);
+}
+
+TEST(list_replace_in_strings, single_element_list_replace_in_strings) {
+    char **list = nullptr;
+    StringListInit(&list);
+    char before[] = "a";
+    char after[] = "b";
+    StringListAdd(list, before);
+
+    ASSERT_EQ(StringListSize(list), 1);
+
+    StringListReplaceInStrings(list, before, after);
+
+    ASSERT_EQ(StringListSize(list), 1);
+
+    char **arr;
+    StringListToString(list, &arr);
+
+    ASSERT_TRUE(arr != nullptr);
+    ASSERT_STREQ(arr[0], "b");
+
+    free_string_array(arr, StringListSize(list));
+    StringListDestroy(&list);
+}
+
+TEST(list_replace_in_strings, multiple_element_list_replace_in_strings) {
+    char **list = nullptr;
+    StringListInit(&list);
+    char before[] = "a";
+    char after[] = "b";
+    StringListAdd(list, before);
+    StringListAdd(list, before);
+    StringListAdd(list, before);
+    StringListAdd(list, after);
+    StringListAdd(list, "c");
+    StringListAdd(list, "d");
+
+    ASSERT_EQ(StringListSize(list), 6);
+
+    StringListReplaceInStrings(list, before, after);
+
+    ASSERT_EQ(StringListSize(list), 6);
+
+    char **arr;
+    StringListToString(list, &arr);
+
+    ASSERT_TRUE(arr != nullptr);
+    ASSERT_STREQ(arr[0], "b");
+    ASSERT_STREQ(arr[1], "b");
+    ASSERT_STREQ(arr[2], "b");
+    ASSERT_STREQ(arr[3], "b");
+    ASSERT_STREQ(arr[4], "c");
+    ASSERT_STREQ(arr[5], "d");
+
+    free_string_array(arr, StringListSize(list));
+    StringListDestroy(&list);
+}
+
+TEST(list_sort, empty_list_sort) {
+    char **list = nullptr;
+    StringListInit(&list);
+    StringListSort(list);
+
+    ASSERT_TRUE(list != nullptr);
+    ASSERT_TRUE(list[WORD] == nullptr);
+    ASSERT_TRUE(reinterpret_cast<char **>(list[1]) == list);
+    ASSERT_TRUE(reinterpret_cast<char **>(list[2]) == list);
+
+    StringListDestroy(&list);
+}
+
+TEST(list_sort, single_element_list_sort) {
+    char **list = nullptr;
+    StringListInit(&list);
+    char element[] = "a";
+    StringListAdd(list, element);
+    StringListSort(list);
+    char** arr;
+    StringListToString(list, &arr);
+
+    ASSERT_TRUE(arr != nullptr);
+    ASSERT_STREQ(arr[WORD], element);
+
+    free_string_array(arr, StringListSize(list));
+    StringListDestroy(&list);
+}
+
+TEST(list_sort, multiple_element_list_sort_sorted) {
+    char **list = nullptr;
+    StringListInit(&list);
+    std::vector<std::string> words = {"a", "b", "c", "d"};
+    for (auto & word: words) {
+        StringListAdd(list, word);
+    }
+
+    StringListSort(list);
+    char** arr;
+    StringListToString(list, &arr);
+
+    ASSERT_TRUE(arr != nullptr);
+    int i=0;
+    for (auto & word: words) {
+        ASSERT_STREQ(arr[i], word.c_str());
+        ++i;
+    }
+
+    free_string_array(arr, StringListSize(list));
+    StringListDestroy(&list);
+}
+
+TEST(list_sort, multiple_element_list_sort_unsorted) {
+    char **list = nullptr;
+    StringListInit(&list);
+    std::vector<std::string> words = {"c", "b", "a", "d"};
+    for (auto & word: words) {
+        StringListAdd(list, word);
+    }
+
+    StringListSort(list);
+    std::sort(words.begin(), words.end());
+    char** arr;
+    StringListToString(list, &arr);
+
+    ASSERT_TRUE(arr != nullptr);
+    int i=0;
+    for (auto & word: words) {
+        ASSERT_STREQ(arr[i], word.c_str());
+        ++i;
+    }
+
+    free_string_array(arr, StringListSize(list));
+    StringListDestroy(&list);
+}
+
+
+TEST(list_sort, multiple_element_list_sort_unsorted_duplicates) {
+    char **list = nullptr;
+    StringListInit(&list);
+    std::vector<std::string> words = {"a", "h", "", " ", "c", "b", "a", "d"};
+    for (auto & word: words) {
+        StringListAdd(list, word);
+    }
+
+    StringListSort(list);
+    std::sort(words.begin(), words.end());
+    char** arr;
+    StringListToString(list, &arr);
+
+    ASSERT_TRUE(arr != nullptr);
+    int i=0;
+    for (auto & word: words) {
+        ASSERT_STREQ(arr[i], word.c_str());
+        ++i;
+    }
+
+    free_string_array(arr, StringListSize(list));
+    StringListDestroy(&list);
+}
+
+TEST(list_complete_test, complete_test_1) {
+    testing::internal::CaptureStdout();
+
+    char **list = nullptr;
+    StringListInit(&list);
+    std::vector<std::string> words = {"a", "h", "", " ", "c", "b", "a", "d"};
+    ASSERT_EQ(StringListSize(list), 0);
+    for (auto & word: words) {
+        StringListAdd(list, word);
+    }
+    ASSERT_EQ(StringListSize(list), 8);
+
+    StringPrint(list);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "a\nh\n\n \nc\nb\na\nd\n");
+
+    StringListRemove(list, "");
+
+    words.erase(std::find(words.begin(),words.end(),""));
+    char** arr;
+    StringListToString(list, &arr);
+    ASSERT_EQ(StringListSize(list), 7);
+    ASSERT_TRUE(arr != nullptr);
+    int i=0;
+    for (auto & word: words) {
+        ASSERT_STREQ(arr[i], word.c_str());
+        ++i;
+    }
+    free_string_array(arr, StringListSize(list));
+
+    ASSERT_EQ(StringListIndexOf(list, " "), 2);
+    ASSERT_EQ(StringListIndexOf(list, "a"), 0);
+    ASSERT_NE(StringListIndexOf(list, " "), 5);
+    ASSERT_EQ(StringListIndexOf(list, "d"), 6);
+
+    StringListRemoveDuplicates(list);
+    words = {"a", "h", " ", "c", "b", "d"};
+    StringListToString(list, &arr);
+    ASSERT_EQ(StringListSize(list), 6);
+    ASSERT_TRUE(arr != nullptr);
+    i=0;
+    for (auto & word: words) {
+        ASSERT_STREQ(arr[i], word.c_str());
+        ++i;
+    }
+
+    free_string_array(arr, StringListSize(list));
+
+    StringListReplaceInStrings(list, "a", "aa");
+    words = {"aa", "h", " ", "c", "b", "d"};
+    StringListToString(list, &arr);
+    ASSERT_EQ(StringListSize(list), 6);
+    ASSERT_TRUE(arr != nullptr);
+    i=0;
+    for (auto & word: words) {
+        ASSERT_STREQ(arr[i], word.c_str());
+        ++i;
+    }
+    free_string_array(arr, StringListSize(list));
+
+    StringListSort(list);
+    std::sort(words.begin(), words.end());
+    StringListToString(list, &arr);
+    ASSERT_EQ(StringListSize(list), 6);
+    ASSERT_TRUE(arr != nullptr);
+    i=0;
+    for (auto & word: words) {
+        ASSERT_STREQ(arr[i], word.c_str());
+        ++i;
+    }
+    free_string_array(arr, StringListSize(list));
+
     StringListDestroy(&list);
 }
